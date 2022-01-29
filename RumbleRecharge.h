9970adf28682969670c49c0d1a93accd27d00216
ZpeedTube <31473172+ZpeedTube@users.noreply.github.com>
2022-01-29 20:23:29 +0200

    (settings) Added programmatic settings
    Creates the needed settings for Rumble Recharge to work.

--- RumbleRecharge.h
+++ RumbleRecharge.h
@@ -8,17 +8,24 @@
 constexpr auto plugin_version = stringify(VERSION_MAJOR) "." stringify(VERSION_MINOR) "." stringify(VERSION_PATCH) "." stringify(VERSION_BUILD);
 
 
-class RumbleRecharge: public BakkesMod::Plugin::BakkesModPlugin
+class RumbleRecharge: public BakkesMod::Plugin::BakkesModPlugin, public BakkesMod::Plugin::PluginSettingsWindow
 {
 	std::shared_ptr<bool> enabled;
 
-	//Boilerplate
+	// Boilerplate
 	virtual void onLoad();
 	virtual void onUnload();
 
 	void ApplyCarMovement(std::string eventName);
 
 	void OnChangeActive(std::string oldValue, CVarWrapper cvar);
+
+	// Settings
+	void RenderSettings() override;
+	std::string GetPluginName() override;
+	void SetImGuiContext(uintptr_t ctx) override;
+
+
 
 };
 
